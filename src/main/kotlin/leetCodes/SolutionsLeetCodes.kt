16e66aa3833a3746f14dfbf0b8903138b3a27c0c
IgorBrizola <igorbrizzola@gmail.com>
2025-06-25 19:17:47 -0300

    feat: solution twoSums

--- src/main/kotlin/leetCodes/SolutionsLeetCodes.kt
+++ src/main/kotlin/leetCodes/SolutionsLeetCodes.kt
@@ -2,9 +2,10 @@ package leetCodes
 
 import org.slf4j.LoggerFactory
 
-private val logger = LoggerFactory.getLogger(SolutionsLeetCodes::class.java)
+private val log = LoggerFactory.getLogger(SolutionsLeetCodes::class.java)
 
 class SolutionsLeetCodes {
+    // PALINDROMIC STRING
     fun longestPalindrome(s: String): String {
         if (s.length < 2) return s
 
@@ -38,26 +39,68 @@ class SolutionsLeetCodes {
         return s.substring(start, end + 1)
     }
 
-    fun twoSum(numbers: IntArray, target: Int): IntArray {
+    // TWO SUM WITH SORTED = TARGET -> O(n)
+    fun twoSumSorted(numbers: IntArray, target: Int): IntArray {
         var left = 0
         var right = numbers.size - 1
-        val numbersSorted = numbers.sorted()
 
         var targetIndex = intArrayOf()
 
-        for (i in 0 until numbersSorted.size - 1) {
-            if (numbersSorted[left] + numbersSorted[right] == target) {
+        for (i in 0 until numbers.size - 1) {
+            if (numbers[left] + numbers[right] == target) {
                 targetIndex = intArrayOf(left, right)
-            } else if (numbersSorted[left] + numbersSorted[right] < target) {
+            } else if (numbers[left] + numbers[right] < target) {
                 left++
-            } else if (numbersSorted[left] + numbersSorted[right] > target) {
+            } else if (numbers[left] + numbers[right] > target) {
                 right--
             } else {
-                throw IllegalArgumentException("Target not found!")
+                throw IllegalArgumentException("No valid solution found.")
             }
         }
 
         return targetIndex
     }
+
+    // TWO SUM WITHOUT SORTED = TARGET -> O(n^2)
+    fun twoSumUnsorted(nums: IntArray, target: Int): IntArray {
+        val map = mutableMapOf<Int, Int>()
+
+                for ((index, num) in nums.withIndex()) {
+
+                    val complement = target - num
+
+                    if (map.containsKey(complement)) {
+                        return intArrayOf(map[complement]!!, index)
+                    }
+
+                    map[num] = index
+                }
+
+        throw IllegalArgumentException("No valid solution found.")
+    }
+
+    fun climbStairs(n: Int): Int {
+        return 1
+    }
+
+    // SEQUENCE FIBONACCI -> O(n)
+    fun fibonacciSequence(num: Int): IntArray {
+        val sequence: MutableList<Int> = mutableListOf(0, 1, 1) // f0, f1, f2
+        val f3 = 3
+
+        for (i in f3..num) {
+            val nextNumberSequence = calculateNextNumberOfSequence(sequence, i)
+            sequence.add(nextNumberSequence)
+        }
+
+        log.info("sequence fibonacci is -> $sequence")
+
+        return sequence.toIntArray()
+    }
+
+    private fun calculateNextNumberOfSequence(currentSequence: List<Int>, fibonacciPosition: Int): Int
+    = ((currentSequence[fibonacciPosition - 1]) + ((currentSequence[fibonacciPosition - 2])))
+
+
 }
 
