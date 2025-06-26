2c3cf275d880d773a80e36d536643a421bb7d4af
IgorBrizola <igorbrizzola@gmail.com>
2025-06-26 15:24:31 -0300

    feat: solution 70. Climbing Stairs

--- src/main/kotlin/leetCodes/SolutionsLeetCodes.kt
+++ src/main/kotlin/leetCodes/SolutionsLeetCodes.kt
@@ -79,9 +79,26 @@ class SolutionsLeetCodes {
         throw IllegalArgumentException("No valid solution found.")
     }
 
+
+
+    // 70. Climbing Stairs
     fun climbStairs(n: Int): Int {
-        return 1
+        if (n <= 3) return n
+
+        var fPrevious = 1
+        var fNext = 1
+
+        val f3 = 2
+
+        for (i in f3..n) {
+            val temp = fPrevious + fNext
+            fPrevious = fNext
+            fNext = temp
+        }
+
+        return fNext
     }
+
 
     // SEQUENCE FIBONACCI -> O(n)
     fun fibonacciSequence(num: Int): IntArray {
@@ -101,6 +118,41 @@ class SolutionsLeetCodes {
     private fun calculateNextNumberOfSequence(currentSequence: List<Int>, fibonacciPosition: Int): Int
     = ((currentSequence[fibonacciPosition - 1]) + ((currentSequence[fibonacciPosition - 2])))
 
+    // 509. Fibonacci Number
+    fun fib(n: Int): Int {
+        if (n < 2) return n
+
+        var f1 = 0
+        var f2 = 1
+
+        for (i in 2..n) {
+            val temp = f1 + f2
+            f1 = f2
+            f2 = temp
+        }
+
+        return f2
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- src/test/kotlin/leetCodes/MainLeetCodesTest.kt
+++ src/test/kotlin/leetCodes/MainLeetCodesTest.kt
@@ -91,18 +91,6 @@ class MainLeetCodesTest {
         assertArrayEquals(expected, result)
     }
 
-
-
-    @Test
-    fun `should return steps for climb to top`(){
-        val stairs = 4
-
-        val result = leetCodes.climbStairs(stairs)
-
-        val expected = listOf(6)
-    }
-
-
     @Test
     fun `should return a sequence of fibonacci`(){
         val n = 19
@@ -112,5 +100,77 @@ class MainLeetCodesTest {
         val expected = intArrayOf(0,1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181)
 
         assertArrayEquals(expected, result)
+    }
+
+    // 70. Climbing Stairs, case one
+    @Test
+    fun `should return steps for climb to top, case one`(){
+        val stairs = 3
+
+        val result = leetCodes.climbStairs(stairs)
+
+        val expected = 3
+
+        assertEquals(expected, result)
+    }
+
+    // 70. Climbing Stairs, case two
+    @Test
+    fun `should return steps for climb to top, case two`(){
+        val stairs = 2
+
+        val result = leetCodes.climbStairs(stairs)
+
+        val expected = 2
+
+        assertEquals(expected, result)
+    }
+
+    // 70. Climbing Stairs, case three
+    @Test
+    fun `should return steps for climb to top, case three`(){
+        val stairs = 6
+
+        val result = leetCodes.climbStairs(stairs)
+
+        val expected = 13
+
+        assertEquals(expected, result)
+    }
+
+    // 509. Fibonacci Number
+    @Test
+    fun `should return fibonacci f(n)`() {
+        val n = 2
+
+        val result = leetCodes.fib(n)
+
+        val expected = 1
+
+        assertEquals(expected, result)
+    }
+
+    // 509. Fibonacci Number case two
+    @Test
+    fun `should return fibonacci f(n), case two`() {
+        val n = 4
+
+        val result = leetCodes.fib(n)
+
+        val expected = 3
+
+        assertEquals(expected, result)
+    }
+
+    // 509. Fibonacci Number case three
+    @Test
+    fun `should return fibonacci f(n), case three`() {
+        val n = 8
+
+        val result = leetCodes.fib(n)
+
+        val expected = 21
+
+        assertEquals(expected, result)
     }
 }
