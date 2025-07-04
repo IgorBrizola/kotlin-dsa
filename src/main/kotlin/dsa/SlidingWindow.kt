447badb8216c286029ac3b228fe7b041bb1e44cd
IgorBrizola <igorbrizzola@gmail.com>
2025-07-04 19:57:49 -0300

    chore: refactor organization ex leetcode

--- src/main/kotlin/dsa/SlidingWindow.kt
+++ src/main/kotlin/dsa/SlidingWindow.kt
@@ -26,19 +26,4 @@ class SlidingWindow {
         }
         return subStringSize
     }
-
-    fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
-        val hashMap = hashMapOf<Int, Int>()
-
-        for (i in nums.indices) {
-            val num = nums[i]
-            if (hashMap.containsKey(num)) {
-                val lastIndex = hashMap[num]!!
-                if (i - lastIndex <= k) return true
-            }
-            hashMap[num] = i
-        }
-
-        return false
-    }
 }
--- src/test/kotlin/dsa/SlidingWindowTest.kt
+++ src/test/kotlin/dsa/SlidingWindowTest.kt
@@ -19,53 +19,5 @@ class SlidingWindowTest {
 
 
 
-    @Test
-    fun `Contains Duplicate II, should return true, case one`(){
-        val nums = intArrayOf(1,2,1)
-        val k = 0
 
-        val result = slidingWindowUseCase.containsNearbyDuplicate(nums, k)
-
-        assertFalse { result }
-    }
-
-    @Test
-    fun `Contains Duplicate II, should return false, case two`(){
-        val nums = intArrayOf(1,2,3,1,2,3)
-        val k = 2
-
-        val result = slidingWindowUseCase.containsNearbyDuplicate(nums, k)
-
-        assertFalse { result }
-    }
-
-    @Test
-    fun `Contains Duplicate II, should return false, case three`(){
-        val nums = intArrayOf(1,2,3,1)
-        val k = 3
-
-        val result = slidingWindowUseCase.containsNearbyDuplicate(nums, k)
-
-        assertTrue { result }
-    }
-
-    @Test
-    fun `Contains Duplicate II, should return true case four`(){
-        val nums = intArrayOf(0,1,2,3,2,5)
-        val k = 3
-
-        val result = slidingWindowUseCase.containsNearbyDuplicate(nums, k)
-
-        assertTrue { result }
-    }
-
-    @Test
-    fun `Contains Duplicate II, should return true case five`(){
-        val nums = intArrayOf(1,0,1,1)
-        val k = 1
-
-        val result = slidingWindowUseCase.containsNearbyDuplicate(nums, k)
-
-        assertTrue { result }
-    }
 }
